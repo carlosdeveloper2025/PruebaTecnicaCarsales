9140075fd2163c35f4631c44294ca733a86a14f1
@CarlosMunozR <carlosmr82@gmail.com>
2025-11-12 16:50:03 -0300

    Agregado frontend y backend

---
+++ BackendPruebaTecnica/BackendPruebaTecnica/Program.cs
@@ -0,0 +1,46 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+// registro HttpClient para la API de Rick and Morty
+builder.Services.AddHttpClient("RickAndMortyClient", client =>
+{
+    client.BaseAddress = new Uri("https://rickandmortyapi.com/api/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
